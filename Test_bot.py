101b6db03007ed23168b4437a66a6eeeab4036de
LordCyber ST. Cyberaticial <s6070122@kmitl.ac.th>
2017-09-20 12:29:18 +0700

    Update Test_bot.py

--- Test_bot.py
+++ Test_bot.py
@@ -13,12 +13,9 @@ from linebot.models import (
  UnfollowEvent, FollowEvent, JoinEvent, LeaveEvent, BeaconEvent
 )
 
-__init__(self, channel_access_token, endpoint='https://api.line.me', timeout=5, http_client=RequestsHttpClient)
-
 line_bot_api = LineBotApi('lK+LiftMxJYs2X3wm0XzcykDvsJO8j8VY9R2xdxLzbjACs6RF0SpXkUP/Q3BLVXePKM6aF+RM7HkK5ExlTbDBS1FicGR1GHLdEakqLcun0+a4QnhW2+wBkQW7yRkUNAsjiU3EeP4khtaxqfDrVTj3gdB04t89/1O/w1cDnyilFU=')
 
 try:
     line_bot_api.push_message('Ucf88fc62d218cfd2f42f358ff16c8551', TextSendMessage(text='Hello World!'))
-    line_bot_api.reply_message(reply_token, TextSendMessage(text='ขอบคุณครับ!'))
 except LineBotApiError as e:
     # error handle
