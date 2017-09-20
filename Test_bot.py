80ff14b4cf5dfce31b592703afe9e2d287bbe6c2
LordCyber ST. Cyberaticial <s6070122@kmitl.ac.th>
2017-09-20 12:26:11 +0700

    Update Test_bot.py

--- Test_bot.py
+++ Test_bot.py
@@ -13,7 +13,7 @@ from linebot.models import (
  UnfollowEvent, FollowEvent, JoinEvent, LeaveEvent, BeaconEvent
 )
 
-__init__(self, channel_access_token, endpoint='https://api.line.me/v2/bot/message/reply', timeout=5, http_client=RequestsHttpClient)
+__init__(self, channel_access_token, endpoint='https://api.line.me', timeout=5, http_client=RequestsHttpClient)
 
 line_bot_api = LineBotApi('lK+LiftMxJYs2X3wm0XzcykDvsJO8j8VY9R2xdxLzbjACs6RF0SpXkUP/Q3BLVXePKM6aF+RM7HkK5ExlTbDBS1FicGR1GHLdEakqLcun0+a4QnhW2+wBkQW7yRkUNAsjiU3EeP4khtaxqfDrVTj3gdB04t89/1O/w1cDnyilFU=')
 
