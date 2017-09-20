2fd2b93ff3d912e64ecc73da2e527c499adbb86c
LordCyber ST. Cyberaticial <s6070122@kmitl.ac.th>
2017-09-20 12:23:40 +0700

    Update code

--- Test_bot.py
+++ Test_bot.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python
 #-*-coding: utf-8 -*-
 
-from linebot import LineBotApi
-from linebot.models import TextSendMessage
-from linebot.exceptions import LineBotApiError
+from linebot import (LineBotApi, WebhookHandler)
+from linebot.models import (
+ MessageEvent, TextMessage, TextSendMessage, ImageSendMessage,
+ SourceUser, SourceGroup, SourceRoom,
+ TemplateSendMessage, ConfirmTemplate, MessageTemplateAction,
+ ButtonsTemplate, URITemplateAction, PostbackTemplateAction,
+ CarouselTemplate, CarouselColumn, PostbackEvent,
+ StickerMessage, StickerSendMessage, LocationMessage, LocationSendMessage,
+ ImageMessage, VideoMessage, AudioMessage,
+ UnfollowEvent, FollowEvent, JoinEvent, LeaveEvent, BeaconEvent
+)
+
+__init__(self, channel_access_token, endpoint='https://api.line.me/v2/bot/message/reply', timeout=5, http_client=RequestsHttpClient)
 
 line_bot_api = LineBotApi('lK+LiftMxJYs2X3wm0XzcykDvsJO8j8VY9R2xdxLzbjACs6RF0SpXkUP/Q3BLVXePKM6aF+RM7HkK5ExlTbDBS1FicGR1GHLdEakqLcun0+a4QnhW2+wBkQW7yRkUNAsjiU3EeP4khtaxqfDrVTj3gdB04t89/1O/w1cDnyilFU=')
 
